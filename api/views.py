3a4969af222fb0d2ee1a949a4fac5be68e672687
Chacha <braverachacha@gmail.com>
2026-01-01 14:46:30 +0300

    Complete weather API endpoint with full data formatting

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, json
+from flask import Blueprint, jsonify
 import requests
 import random
 import os 
@@ -9,13 +9,35 @@ views = Blueprint('views',__name__)
 
 kenyaCities = ["Nairobi","Mombasa","Kisumu","Nakuru","Eldoret","Thika","Machakos","Meru","Embu","Nyeri","Kericho","Bomet","Narok","Naivasha","Kitale","Bungoma","Kakamega","Busia","Siaya","Homa Bay","Migori","Kisii","Nyamira","Rongo","Oyugis","Ahero","Voi","Taveta","Wundanyi","Malindi","Kilifi","Lamu","Garissa","Wajir","Mandera","Isiolo","Marsabit","Moyale","Kapenguria","Lodwar"];
 
+
 @views.route('/data/')
 def weather_data():
   city = random.choice(kenyaCities)
   url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
-  response = requests.get(f'{url}')
+  response = requests.get(url)
   if response.status_code == 200:
     values = response.json()
-    return jsonify(values)
+    formated_data = data_format(values)
+    return jsonify(formated_data)
   else:
-    return jsonify({'error': 'Error occured!'})
+    return jsonify({'error': 'Error occured!'}), 400
+    
+def data_format(data):
+  return {
+    'city': data['name'],
+    'temp': data['main']['temp'],
+    'temp_max': data['main']['temp_max'],
+    'temp_min': data['main']['temp_min'],
+    'feels_like': data['main']['feels_like'],
+    'sea_level': data['main'].get('sea_level', 'N/A'),
+    'pressure': data['main']['pressure'],
+    'humidity': data['main']['humidity'],
+    'grnd_level': data['main'].get('grnd_level', 'N/A'),
+    'description': data['weather'][0]['description'],
+    'main': data['weather'][0]['main'],
+    'icon': data['weather'][0]['icon'],
+    'wind': data['wind']['speed'],
+    'country': data['sys']['country'],
+    'sunrise': data['sys']['sunrise'],
+    'sunset': data['sys']['sunset'],
+  }
